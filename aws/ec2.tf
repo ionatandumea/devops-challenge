fe64c251cb011bc9648fba151bded21882685445
Ionatan Dumea <dumeaionatan9@gmail.com>
2024-06-09 15:49:58 +0300

    chore: Update Docker build and deploy workflows to use docker-compose for deployment

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -13,6 +13,9 @@ resource "aws_instance" "ec2-node-1" {
   sudo groupadd docker
   sudo usermod -aG docker ubuntu
   newgrp docker
+
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
   EOF
 
   tags = {
